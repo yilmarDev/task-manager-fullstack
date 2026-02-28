23307f09addbf791a28d6998c3dcfe9acf8bf280
Yilmar Lopez Ospina <yilmardev@gmail.com>
2026-02-28 07:34:19 -0500

    🚧 Adding task dependencies

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,9 +1,11 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.db import get_db
 from src.repositories.user_repository import UserRepository
 from src.services.user_services import UserService
-from src.db import get_db
+from src.repositories.task_repository import TaskRepository
+from src.services.task_services import TaskService
 
 
 def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserRepository:
@@ -16,3 +18,15 @@ def get_user_service(
 ) -> UserService:
     """Factory: Provide UserService"""
     return UserService(repo)
+
+
+def get_task_repository(db: AsyncSession = Depends(get_db)) -> TaskRepository:
+    """Factory: Provide TaskRepository"""
+    return TaskRepository(db)
+
+
+def get_task_service(
+    repo: TaskRepository = Depends(get_task_repository),
+) -> TaskService:
+    """Factory: Provide TaskService"""
+    return TaskService(repo)
